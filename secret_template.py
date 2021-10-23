bec792ea89d8cc9641837ac2cf31689d59e6c6e2
Brian Chhour <bchhour@brians-macbook.wifi.local.cmu.edu>
2021-10-23 14:44:01 -0400

    Added secret_template.py

---
+++ secret_template.py
@@ -0,0 +1,7 @@
+api_key = 'Your API key goes here.'
+domain = "scottylabs.org"
+sender = "ScottyLabs <hello@" + domain + ">"
+reply_to = "ScottyLabs <hello@" + domain + ">"
+recepients_general = [ "sl-public@lists.andrew.cmu.edu" ]
+recepients_testing = [ "sl-email-testing@lists.andrew.cmu.edu" ]
+recepients_tech = [ "sl-tech@lists.andrew.cmu.edu" ]
