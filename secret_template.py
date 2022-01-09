847668a91d47eb0cdc35622d91fae20b5dfb4156
Anuda Weerasinghe <anuda@live.com>
2022-01-09 22:57:48 +0530

    changed sender in secret template

--- secret_template.py
+++ secret_template.py
@@ -1,7 +1,7 @@
 api_key = 'Your Mailgun API key goes here.'
-domain = "scottylabs.org"
-sender = "ScottyLabs <hello@" + domain + ">"
-reply_to = "ScottyLabs <hello@" + domain + ">"
+domain = "tartanhacks.com"
+sender = "TartanHacks 2022 <hello@" + domain + ">"
+reply_to = "TartanHacks 2022 <hello@" + domain + ">"
 recepients_general = [ "sl-public@lists.andrew.cmu.edu" ]
 recepients_testing = [ "sl-email-testing@lists.andrew.cmu.edu" ]
 recepients_tech = [ "sl-tech@lists.andrew.cmu.edu" ]
